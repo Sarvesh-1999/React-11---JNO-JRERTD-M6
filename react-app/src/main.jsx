0938d810659230ed5afdcda677880849c5c826ae
Sarvesh <>
2026-05-01 12:34:18 +0530

    React Project structure setup

---
+++ react-app/src/main.jsx
@@ -0,0 +1,16 @@
+//! REACT WAY
+import { createRoot } from "react-dom/client";
+createRoot(document.getElementById("root")).render(
+  <>
+    <h1>Hello React</h1>
+    <h2>Hiii</h2>
+  </>,
+);
+
+//<></> --> Fragments
+
+//! JS WAY
+// const divTag = document.getElementById("root");
+// const h1tag = document.createElement("h1");
+// h1tag.textContent = "Hello React! using JS way";
+// divTag.append(h1tag);
